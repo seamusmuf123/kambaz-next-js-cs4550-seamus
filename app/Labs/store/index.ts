de80d2f5da9d732e63a8df8333a8eacb7d4dec44
seamusmuf123 <mufarinya.se@northeastern.edu>
2025-11-04 03:33:39 -0500

    changed and remove necessary files in lab4

--- app/Labs/store/index.ts
+++ app/Labs/store/index.ts
@@ -3,6 +3,7 @@ import helloReducer from "../Lab4/ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "../Lab4/ReduxExamples/CounterRedux/counterReducer";
 import addReducer from "../Lab4/ReduxExamples/AddRedux/addReducer";
 import todosReducer from "../Lab4/ReduxExamples/todos/todosReducer";
+
 const store = configureStore({
   reducer: {
     helloReducer,
@@ -11,4 +12,8 @@ const store = configureStore({
     todosReducer,
   },
 });
+
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
